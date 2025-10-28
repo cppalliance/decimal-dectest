1145028a98dc38f317d0852bac2db325438b9ed5
Matt Borland <matt@mattborland.com>
2025-10-28 10:28:08 +0100

    Add test harness placeholder

---
+++ test_harness.hpp
@@ -0,0 +1,22 @@
+// Copyright 2025 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#ifndef BOOST_DECIMAL_DECTEST_TEST_HARNESS_HPP
+#define BOOST_DECIMAL_DECTEST_TEST_HARNESS_HPP
+
+#include <boost/decimal.hpp>
+#include <boost/core/lightweight_test.hpp>
+#include "where_file.hpp"
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+
+template <typename Function>
+inline void test_harness(const std::string& file_path, Function f)
+{
+
+}
+
+#endif // BOOST_DECIMAL_DECTEST_TEST_HARNESS_HPP
