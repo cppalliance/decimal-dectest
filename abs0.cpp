fcd0e8d3d9ebdb974cb5b93d6f662fb49ffb6db6
Matt Borland <matt@mattborland.com>
2025-10-28 10:37:16 +0100

    Add first test

---
+++ abs0.cpp
@@ -0,0 +1,14 @@
+// Copyright 2025 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/decimal.hpp>
+#include <boost/core/lightweight_test.hpp>
+#include "test_harness.hpp"
+#include <functional>
+
+int main()
+{
+    test_harness("dectest0/abs0.decTest", [](const auto x) { return boost::decimal::abs(x); });
+    return boost::report_errors();
+}
