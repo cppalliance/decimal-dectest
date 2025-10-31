b15db5cad9385ec32c548b4050495b3f8dd86c9d
Matt Borland <matt@mattborland.com>
2025-10-31 16:05:05 +0100

    Add driver file

---
+++ test_compare.cpp
@@ -0,0 +1,14 @@
+// Copyright 2025 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/decimal.hpp>
+#include <boost/core/lightweight_test.hpp>
+#include "test_harness.hpp"
+
+int main()
+{
+    test_comparisons("dectest0/compare0.decTest", "compare");
+
+    return boost::report_errors();
+}
