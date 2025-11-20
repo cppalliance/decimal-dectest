15ba8938a611204866e783aa5e3ae38ef00edf74
Matt Borland <matt@mattborland.com>
2025-11-20 11:58:54 +0100

    Require rounding support to run comprehensive addition testing

--- test_add.cpp
+++ test_add.cpp
@@ -10,8 +10,14 @@ int main()
 {
     test_two_arg_harness("dectest0/add0.decTest", "add", [](const auto x, const auto y) { return x + y; });
 
+    // Requires rounding-mode changes
+    #ifndef BOOST_DECIMAL_NO_CONSTEVAL_DETECTION
+
+    boost::decimal::fesetround(boost::decimal::rounding_mode::fe_dec_default);
     std::cerr << std::setprecision(std::numeric_limits<boost::decimal::decimal64_t>::max_digits10);
-    test_two_arg_harness("dectest/ddAdd.decTest", "add", [](const auto x, const auto y) { return x + y; });
+    test_two_arg_harness<true>("dectest/ddAdd.decTest", "add", [](const auto x, const auto y) { return x + y; });
+
+    #endif // BOOST_DECIMAL_NO_CONSTEVAL_DETECTION
 
     return boost::report_errors();
 }
