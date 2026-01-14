91e6a01c09b5d2d5184b1eea42c1b7bdeb94d3b0
Matt Borland <matt@mattborland.com>
2026-01-14 13:59:45 -0500

    Test min0

---
+++ test_min.cpp
@@ -0,0 +1,15 @@
+// Copyright 2026 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/decimal.hpp>
+#include <boost/core/lightweight_test.hpp>
+#include "test_harness.hpp"
+
+int main()
+{
+    std::cerr << std::setprecision(17);
+    test_two_arg_harness("dectest0/min0.decTest", "min", [](const auto x, const auto y) { return boost::decimal::fmin(x, y); });
+
+    return boost::report_errors();
+}
