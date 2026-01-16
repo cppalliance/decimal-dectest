2b03230efaf78b98496925ad36ee6346117e5cd1
Matt Borland <matt@mattborland.com>
2026-01-16 16:33:51 -0500

    Add testing of plus operator

---
+++ test_plus.cpp
@@ -0,0 +1,14 @@
+// Copyright 2026 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/decimal.hpp>
+#include <boost/core/lightweight_test.hpp>
+#include "test_harness.hpp"
+
+int main()
+{
+    test_one_arg_harness("dectest0/plus0.decTest", "plus", [](const auto x) { return +x; });
+
+    return boost::report_errors();
+}
