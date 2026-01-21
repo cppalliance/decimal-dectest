bc1b329e5b3ff4809b121cdfe52d4964e899a6eb
Matt Borland <matt@mattborland.com>
2026-01-21 13:05:33 -0500

    Add 64-bit multiplication dectest

--- test_multiply.cpp
+++ test_multiply.cpp
@@ -14,5 +14,11 @@ int main()
     std::cerr << std::setprecision(34);
     test_two_arg_harness("dectest/multiply.decTest", "multiply", [](const auto x, const auto y) { return x * y; });
 
+    #ifndef BOOST_DECIMAL_NO_CONSTEVAL_DETECTION
+
+    test_two_arg_harness<true>("dectest/ddMultiply.decTest", "multiply", [](const auto x, const auto y) { return x * y; });
+
+    #endif
+
     return boost::report_errors();
 }
