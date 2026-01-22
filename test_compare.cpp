bd4ab9b126271c9170abef05e96bc8436dc96d4c
Matt Borland <matt@mattborland.com>
2026-01-22 13:04:45 -0500

    Add 128-bit comparisons testing

--- test_compare.cpp
+++ test_compare.cpp
@@ -11,9 +11,11 @@ int main()
     test_comparisons("dectest0/compare0.decTest", "compare");
     test_comparisons("dectest/compare.decTest", "compare");
     test_comparisons("dectest/ddCompare.decTest", "compare");
+    test_comparisons("dectest/dqCompare.decTest", "compare");
 
     test_comparisons("dectest/comparesig.decTest", "comparesig");
     test_comparisons("dectest/ddComparesig.decTest", "comparesig");
+    test_comparisons("dectest/dqComparesig.decTest", "comparesig");
 
     return boost::report_errors();
 }
