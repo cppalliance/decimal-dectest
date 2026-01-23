fc03b18f3f780f7f2a1b58e95e8488b11daa164c
Matt Borland <matt@mattborland.com>
2026-01-23 13:55:54 -0500

    Add test remainder

---
+++ test_remainder.cpp
@@ -0,0 +1,15 @@
+// Copyright 2025 Matt Borland
+// Distributed under the Boost Software License, Version 1.0.
+// https://www.boost.org/LICENSE_1_0.txt
+
+#include <boost/decimal.hpp>
+#include <boost/core/lightweight_test.hpp>
+#include "test_harness.hpp"
+
+int main()
+{
+    std::cerr << std::setprecision(17);
+    test_two_arg_harness("dectest0/remainder0.decTest", "remainder", [](const auto x, const auto y) { return x % y; });
+
+    return boost::report_errors();
+}
